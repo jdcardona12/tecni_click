7bf92a8647b0cbbbf0f757d0ec1ea6c2975dc891
santyagoLM <165937601+santyagoLM@users.noreply.github.com>
2026-09-14 22:46:37 -0500

    Update main.py

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+
 from routers import auth
+from routers import calificacion
+
 
 app = FastAPI()
 
+
 app.include_router(auth.router)
+app.include_router(calificacion.router)
+
 
 @app.get("/")
 def inicio():
-    return {"mensaje": "API de TecniClick funcionando"}
+    return {
+        "mensaje": "API de TecniClick funcionando"
+    }
